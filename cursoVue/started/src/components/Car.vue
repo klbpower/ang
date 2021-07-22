62494766559e38d0d48a75e3a0b83adadab1510e
Sergio C <sergiocalvosos@gmail.com>
2021-07-22 20:46:42 +0200

    Test

---
+++ cursoVue/started/src/components/Car.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Coche </h2>
+    <Test />
+
+</template>
+
+<script>
+import Test from "./Test";
+
+export default{
+    components: {
+        Test
+    }
+}; 
+</script>
+
+<style scoped>
+
+</style>
---
+++ cursoVue/started/src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>
+        Componente Test
+    </p>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
