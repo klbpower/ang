cc3f63920c6f75a0254df8d440f95d64b6cae83e
Sergio C <sergiocalvosos@gmail.com>
2021-07-22 21:05:07 +0200

    Variables C. Options

--- cursoVue/started/src/components/Car.vue
+++ cursoVue/started/src/components/Car.vue
@@ -1,16 +1,28 @@
 <template>
     <h2>Coche </h2>
-    <Test />
-
+<p>Marca: {{brand}}</p>
+<p>Color: {{color[2]}}</p>
+<p>Precio: {{price}}</p>
+<p>Potencia: {{power}}</p>
+{{message.title}} - {{message.text}}
 </template>
 
 <script>
-import Test from "./Test";
 
 export default{
-    components: {
-        Test
-    }
+   data(){
+       return {
+           brand: "Audi",
+           model:"A4",
+           color:["Blue", "Orange", "Yellow", "Red", "Pink", "Brown", "Green", "Gray", "Black", "White"],
+           price:38000,
+           power:180,
+           message: {
+               title: "Hello World",
+               text: "Good Bye",
+           },
+       };
+   },
 }; 
 </script>
 
